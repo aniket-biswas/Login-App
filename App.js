8d5bf4165ff9d4b006e4b7f4103aae8e220ccce2
Aniket Biswas <74890960+aniket-biswas@users.noreply.github.com>
2021-07-08 22:27:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './components/Login';
 import SignUp from './components/signUp';
+import StackNavigation from './components/StackNavigation';
 
 export default function App() {
   return (
-    <SignUp />
+    <StackNavigation />
 
 
   )
